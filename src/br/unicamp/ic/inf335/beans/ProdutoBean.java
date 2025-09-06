b1913777e0c931941f61c9e5e604da2f1095296e
PedroT <pedro.ceriotti@hotmailcom>
2025-09-06 09:25:21 -0300

    Fixes

--- src/br/unicamp/ic/inf335/beans/ProdutoBean.java
+++ src/br/unicamp/ic/inf335/beans/ProdutoBean.java
@@ -66,12 +66,7 @@ public class ProdutoBean implements java.io.Serializable, Comparable<ProdutoBean
 	
 	@Override
 	public int compareTo(ProdutoBean p) {
-		if (valor > p.getValor()) {
-			return 1;
-		} else if (valor > p.getValor()) {
-			return -1;
-		} else
-		    return 0;
+		return valor.compareTo(p.getValor());
 	}
 	
 	
